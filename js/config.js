78260479a5f88bac36cad46a89519b2cfa9d3a08
Mathieu Charron <mathieu@hyberia.ca>
2012-02-27 17:09:58 -0500

    Removed API key.

--- js/config.js
+++ js/config.js
@@ -4,15 +4,15 @@
 var sites = [
 	{ name: "Giant Bomb", configs: [
 		"http://api.giantbomb.com",
-		"fbf5f21d4d477e2da35a1eb5391340e197ed5d68"
+		""
 	]},
 	{ name: "Tested", configs: [
 		"http://api.tested.com",
-		"9ac07c06b6e449c46d175ba97acb0712838f7b1f"
+		""
 	]},
 	{ name: "Screened", configs: [
-        "http://api.screend.com",
-        "fbf5f21d4d477e2da35a1eb5391340e197ed5d68" // Invalid API Key
+       "http://api.screend.com",
+        "" // Invalid API Key
     ]}
 ];
 var configs = [];
